4de9ef033ec57b8dca2039f497428d22558d4ede
Sergey Tupitsyn <cyn78@mail.ru>
2023-11-13 21:50:20 +0300

    stage 3 create splitToLemmas in LemmaSearch

--- src/main/java/searchengine/Main.java
+++ src/main/java/searchengine/Main.java
@@ -2,6 +2,7 @@ package searchengine;
 
 import org.apache.lucene.morphology.LuceneMorphology;
 import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
+import searchengine.services.LemmaSearch;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -14,34 +15,10 @@ public class Main {
             "Кавказа.";
 
     public static void main(String[] args) throws IOException {
-        /*LuceneMorphology luceneMorph =
-                new RussianLuceneMorphology();
-        List<String> wordBaseForms =
-                luceneMorph.getNormalForms("леса");
-        wordBaseForms.forEach(System.out::println);*/
-        HashMap<String,Integer> lemmas = splitToLemmas(text);
+        LemmaSearch lemmaSearch = new LemmaSearch();
+        HashMap<String, Integer> lemmas = lemmaSearch.splitToLemmas(text);
         for (String lemma : lemmas.keySet()) {
-            System.out.println(lemma + " " + lemmas.get(lemma));
+            System.out.println(lemma + " = " + lemmas.get(lemma));
         }
-    }
-
-    public static HashMap<String, Integer> splitToLemmas(String text) throws IOException {
-
-        String regex = "[0-9,.;:!?]";
-        text = text.replaceAll(regex,"");
-        text = text.toLowerCase();
-        String[] words = text.split("\\s+");
-        LuceneMorphology luceneMorph = new RussianLuceneMorphology();
-        HashMap<String, Integer> lemmas = new HashMap<>();
-        for (int i = 0; i < words.length; i++) {
-            List<String> wordBaseForms = luceneMorph.getNormalForms(words[i]);
-            wordBaseForms.forEach(word -> {
-                if (lemmas.containsKey(word)) {
-                    int lemmasCount = lemmas.get(word);
-                    lemmas.replace(word, lemmasCount, lemmasCount + 1 );
-                } else lemmas.put(word, 1);
-            });
-        }
-        return lemmas;
     }
 }
---
+++ src/main/java/searchengine/services/LemmaSearch.java
@@ -0,0 +1,53 @@
+package searchengine.services;
+
+import org.apache.lucene.morphology.LuceneMorphology;
+import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+public class LemmaSearch {
+
+    private static final String[] functionWords = new String[]{"МЕЖД", "ПРЕДЛ", "СОЮЗ"};
+
+    public LemmaSearch() {
+    }
+
+    public HashMap<String, Integer> splitToLemmas(String text) throws IOException {
+
+        String[] words = text.toLowerCase().replaceAll("([^а-я\\s])","").trim().split("\\s+");
+        LuceneMorphology luceneMorph = new RussianLuceneMorphology();
+        HashMap<String, Integer> lemmas = new HashMap<>();
+        for (String word : words) {
+            if (word.isBlank()) {
+                continue;
+            }
+            List<String> wordMorphInfo = luceneMorph.getMorphInfo(word);
+            if (isFunctionWords(wordMorphInfo)) {
+                continue;
+            }
+            List<String> wordBaseForms = luceneMorph.getNormalForms(word);
+            if (wordBaseForms.isEmpty()) {
+                continue;
+            }
+            String wordBaseForm = wordBaseForms.get(0);
+            if (lemmas.containsKey(wordBaseForm)) {
+                int lemmasCount = lemmas.get(wordBaseForm);
+                lemmas.replace(wordBaseForm, lemmasCount, lemmasCount + 1 );
+            } else lemmas.put(wordBaseForm, 1);
+        }
+        return lemmas;
+    }
+
+    public boolean isFunctionWords(List<String> wordMorphInfo) {
+        for (String functionWord : functionWords) {
+            if (wordMorphInfo.get(0).toUpperCase().contains(functionWord)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
